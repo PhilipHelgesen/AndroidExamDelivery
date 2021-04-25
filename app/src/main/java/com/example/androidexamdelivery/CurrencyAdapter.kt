f237df77a5586d1780f2b61da5919adda824ab4e
abhil <abhilash.kamboj@gmail.com>
2021-04-25 22:31:22 +0200

    small tweaks to page 2

--- app/src/main/java/com/example/androidexamdelivery/CurrencyAdapter.kt
+++ app/src/main/java/com/example/androidexamdelivery/CurrencyAdapter.kt
@@ -4,12 +4,8 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.androidexamdelivery.databinding.ItemCurrencyBinding
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_currency.view.*
 
 
@@ -31,25 +27,24 @@ class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.CurrencyViewHolder>
 
         holder.itemView.apply{
             cryptoName.text = currencyList[position].name
-            cryptoRate.text = currencyList[position].priceUsd
+            cryptoRate.text = currencyList[position].priceUsd.substring(0,10)
             cryptoSymbol.text = currencyList[position].symbol
-            cryptoRateChange.text = currencyList[position].changePercent24Hr
+            cryptoRateChange.text = currencyList[position].changePercent24Hr.substring(0,6)
 
             currencyId = currencyList[position].symbol.toLowerCase()
             val imageUrl = "https://static.coincap.io/assets/icons/$currencyId@2x.png"
             Glide.with(this).load(imageUrl).into(cryptoImage)
 
 
-//            if (data!! >= "0.00"){
-//                cryptoRateChange.setTextColor(
-//                    Color.GREEN)
-//            }else{
-//                cryptoRateChange.setTextColor(
-//                    Color.RED)
-//            }
-
+            if (currencyList[position].changePercent24Hr!! >= "0.00"){
+                cryptoRateChange.setTextColor(
+                    Color.GREEN)
+            }
+            else{
+                cryptoRateChange.setTextColor(
+                    Color.RED)
+            }
         }
-
     }
 
 
