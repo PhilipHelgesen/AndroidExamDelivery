fe9aa37b22f2cbdac1af7f0bb14bdcf96ca819d6
Philip Helgesen <55789033+PhilipHelgesen@users.noreply.github.com>
2021-05-01 15:48:24 +0200

    fixed lateinit error on adapter

--- app/src/main/java/com/example/androidexamdelivery/transaction/history/TransactionListFragment.kt
+++ app/src/main/java/com/example/androidexamdelivery/transaction/history/TransactionListFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidexamdelivery.R
 import com.example.androidexamdelivery.databinding.TransactionListFragmentBinding
+import com.example.androidexamdelivery.transaction.buy.BuyCurrencyScreenFragment
 
 class TransactionListFragment : Fragment(R.layout.transaction_list_fragment) {
 
@@ -15,7 +16,13 @@ class TransactionListFragment : Fragment(R.layout.transaction_list_fragment) {
         fun newInstance() = TransactionListFragment()
     }
 
-    private lateinit var adapter: WalletAdapter
+    private val adapter = WalletAdapter { wallet ->
+        parentFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            BuyCurrencyScreenFragment.newInstance(walletID = wallet.id))
+            .addToBackStack("BuyScreenFragment")
+            .commit()
+    }
     private lateinit var binding: TransactionListFragmentBinding
     private val viewModel: TransactionListViewModel by viewModels()
 
@@ -37,8 +44,9 @@ class TransactionListFragment : Fragment(R.layout.transaction_list_fragment) {
     }
 
     private fun configureList() {
-        binding.TransactionList.adapter = adapter
         binding.TransactionList.layoutManager = LinearLayoutManager(requireContext())
+        binding.TransactionList.adapter = adapter
+
     }
 
 }
