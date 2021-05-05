b6d737da7c81f6777aec95030ddab98f1f35aa38
abhil <abhilash.kamboj@gmail.com>
2021-05-05 17:41:21 +0200

    removed redudant data

--- app/src/main/java/com/example/androidexamdelivery/Asset.kt
+++ app/src/main/java/com/example/androidexamdelivery/Asset.kt
@@ -20,13 +20,8 @@ data class Data(
         val rank: String,
         val symbol: String,
         val name: String,
-        //val supply: String,
-        //val maxSupply: String?,
-        //val marketCapUsd: String,
-        //val volumeUsd24Hr: String,
         val priceUsd: String,
         val changePercent24Hr: String,
-       // val vwap24Hr: String
         )
 
 
