2f4c70fb24a7a34e2f0dfc10e23cfcb90091dd36
abhil <abhilash.kamboj@gmail.com>
2021-05-05 19:01:04 +0200

    click listener activity

--- app/src/main/java/com/example/androidexamdelivery/activity/CryptoListMainActivity.kt
+++ app/src/main/java/com/example/androidexamdelivery/activity/CryptoListMainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.androidexamdelivery.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidexamdelivery.Data
+import com.example.androidexamdelivery.R
 import com.example.androidexamdelivery.viewmodel.ViewAssetsViewModel
 import com.example.androidexamdelivery.adapter.CurrencyAdapter
 import com.example.androidexamdelivery.databinding.ActivityMainBinding
@@ -24,7 +28,7 @@ class CryptoListMainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.allAssetsList.observe(this){ assetList ->
+        viewModel.allAssetsList.observe(this) { assetList ->
             adapter.setCurrencyList(assetList.data)
         }
         viewModel.fetchCurrencyData()
@@ -32,6 +36,14 @@ class CryptoListMainActivity : AppCompatActivity() {
         binding.currencyRecyclerView.adapter = adapter
         binding.currencyRecyclerView.layoutManager = LinearLayoutManager(this)
 
+        val headerElement = findViewById<TextView>(R.id.header_click_element)
+        headerElement.setOnClickListener {
+            val intent = Intent(this, PortfolioActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 
 }
+
